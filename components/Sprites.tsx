6e7f0d7d29630437eaad9f8d23b26671866d5da5
qbreis <qbreis@gmail.com>
2023-04-05 13:22:31 +0200

    style: applies prettier rules

--- components/Sprites.tsx
+++ components/Sprites.tsx
@@ -1,17 +1,15 @@
 export default function Sprites({ handleClick, sprites }: any) {
-  return (
-    <div className="sprites" onClick={handleClick}>
-      {sprites.map((sprite:any, key:any) => {
-        return (
-          <div
-            key={key}
-            className="location-circle"
-            style={{ left: sprite.clientX, top: sprite.clientY }}
-          >
-            {key}
-          </div>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="sprites" onClick={handleClick}>
+            {sprites.map((sprite: any, key: any) => {
+                return (
+                    <div
+                        key={key}
+                        className="location-circle"
+                        style={{ left: sprite.clientX, top: sprite.clientY }}
+                    ></div>
+                );
+            })}
+        </div>
+    );
 }
--- includes/functions.ts
+++ includes/functions.ts
@@ -10,48 +10,38 @@ will return:
 */
 
 export function timeToPercentage(thisArray: any) {
-  const result: any = [];
+    const result: any = [];
 
-  thisArray.forEach(function (value: any) {
-    result.push(
-      (value * 100) /
-        thisArray.reduce(function (a: any, b: any) {
-          return a + b;
-        }, 0)
-    );
-  });
+    thisArray.forEach(function (value: any) {
+        result.push(
+            (value * 100) /
+                thisArray.reduce(function (a: any, b: any) {
+                    return a + b;
+                }, 0)
+        );
+    });
 
-  return result;
+    return result;
 }
 
-export function comparePercentages(array1: any, array2: any) {
-  let sequencesFound = 0;
-  array1.forEach(function (value: any, index: any) {
-    // console.log(value, array2[index]);
-    if (value && array2[index]) {
-      if (value > array2[index] - 5 && value < array2[index] + 5)
-        sequencesFound++;
-    }
-  });
-  // console.log(array1.length+' --- '+sequencesFound);
-  return array1.length === sequencesFound;
+export function comparePercentages(props: any) {
+    let sequencesFound = 0;
+    props.array1.forEach(function (value: any, index: any) {
+        // console.log(value, array2[index]);
+        if (value && props.array2[index]) {
+            if (
+                value > props.array2[index] - 5 &&
+                value < props.array2[index] + 5
+            ) {
+                sequencesFound++;
+            }
+        }
+    });
+    // console.log(array1.length+' --- '+sequencesFound);
+    return props.array1.length === sequencesFound;
 }
 
-export function comparePercentagesWithProps(props: any) {
-  let sequencesFound = 0;
-  props.array1.forEach(function (value: any, index: any) {
-    // console.log(value, array2[index]);
-    if (value && props.array2[index]) {
-      if (value > props.array2[index] - 5 && value < props.array2[index] + 5)
-        sequencesFound++;
-    }
-  });
-  // console.log(array1.length+' --- '+sequencesFound);
-  return props.array1.length === sequencesFound;
-}
-
-
-comparePercentagesWithProps.propTypes = {
-  array1: PropTypes.array.isRequired,
-  array2: PropTypes.array.isRequired,
-}
+comparePercentages.propTypes = {
+    array1: PropTypes.array.isRequired,
+    array2: PropTypes.array.isRequired
+};
